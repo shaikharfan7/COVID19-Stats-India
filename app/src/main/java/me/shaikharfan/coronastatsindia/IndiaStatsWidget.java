655b3d88f92120c6df5816889a043cbd6f3228dc
shaikharfan7 <shaikharfan7@gmail.com>
2020-03-23 18:54:56 +0530

    Fixed Thread issue

--- app/src/main/java/me/shaikharfan/coronastatsindia/IndiaStatsWidget.java
+++ app/src/main/java/me/shaikharfan/coronastatsindia/IndiaStatsWidget.java
@@ -22,78 +22,77 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static android.content.ContentValues.TAG;
 
 
 /**
  * Implementation of App Widget functionality.
- * App Widget Configuration implemented in {@link IndiaStatsWidgetConfigureActivity IndiaStatsWidgetConfigureActivity}
  */
 public class IndiaStatsWidget extends AppWidgetProvider {
 
     public static String countryCases, countryCured, countryDeaths;
 
 
-    //Thread-Method used from https://github.com/google/gson
-
-      static Thread thw = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            //GITHUB - https://github.com/amodm/api-covid19-in
-            String sURL = "https://api.rootnet.in/covid19-in/stats/latest"; //DATA SOURCE URL
-
-            // Connect to the URL using java's native library
-
-            URL url = null;
-            try {
-                url = new URL(sURL);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            URLConnection request = null;
-            try {
-                request = url.openConnection();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                request.connect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            // Convert to a JSON object to print data
-            JsonParser jp = new JsonParser(); //from gson
-            JsonElement root = null; //Convert the input stream to a json element
-            try {
-                root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
-            JsonObject data = rootobj.getAsJsonObject("data"); //grabbing Json Data
-            JsonObject cdata = data.getAsJsonObject("summary");//grabbing country data
-            Log.d(TAG, "run: In WidgetClass Thread");
-            //Setting data in global variables
-            countryCases = cdata.get("total").toString();
-            countryCured = cdata.get("discharged").toString();
-            countryDeaths = cdata.get("deaths").toString();
-            Log.d(TAG, "run: In WidgetClass Thread"+countryCases);
-            Log.d(TAG, "run: In WidgetClass Thread"+countryCured);
-            Log.d(TAG, "run: In WidgetClass Thread"+countryDeaths);
-
-        }
-    });
 
 
 
      static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,int appWidgetId)
      {
-        CharSequence widgetText = IndiaStatsWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
-        // Construct the RemoteViews object
+         //Thread-Method used from https://github.com/google/gson
+
+          Thread thw = new Thread(new Runnable() {
+             @Override
+             public void run() {
+                 //GITHUB - https://github.com/amodm/api-covid19-in
+                 String sURL = "https://api.rootnet.in/covid19-in/stats/latest"; //DATA SOURCE URL
+
+                 // Connect to the URL using java's native library
+
+                 URL url = null;
+                 try {
+                     url = new URL(sURL);
+                 } catch (MalformedURLException e) {
+                     e.printStackTrace();
+                 }
+                 URLConnection request = null;
+                 try {
+                     request = url.openConnection();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+                 try {
+                     request.connect();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+
+                 // Convert to a JSON object to print data
+                 JsonParser jp = new JsonParser(); //from gson
+                 JsonElement root = null; //Convert the input stream to a json element
+                 try {
+                     root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+                 JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+                 JsonObject data = rootobj.getAsJsonObject("data"); //grabbing Json Data
+                 JsonObject cdata = data.getAsJsonObject("summary");//grabbing country data
+                 Log.d(TAG, "run: In WidgetClass Thread");
+                 //Setting data in global variables
+                 countryCases = cdata.get("total").toString();
+                 countryCured = cdata.get("discharged").toString();
+                 countryDeaths = cdata.get("deaths").toString();
+                 Log.d(TAG, "run: In WidgetClass Thread"+countryCases);
+                 Log.d(TAG, "run: In WidgetClass Thread"+countryCured);
+                 Log.d(TAG, "run: In WidgetClass Thread"+countryDeaths);
+             }
+         });
+
+         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.india_stats_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text,"COVID19 Stats India");
         int i=3;
          try {
              thw.setPriority(10);
@@ -138,10 +137,8 @@ public class IndiaStatsWidget extends AppWidgetProvider {
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         // When the user deletes the widget, delete the preference associated with it.
-        for (int appWidgetId : appWidgetIds) {
-            IndiaStatsWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
             Toast.makeText(context, "Add widget to stay informed !", Toast.LENGTH_SHORT).show();
-        }
+
     }
 
     @Override
